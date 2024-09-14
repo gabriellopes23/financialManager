b618b64eef6cb08419d1b3ca642c365d4a5eae48
gabriellopes23 <lopessantosgabriel@outlook.com>
2024-09-14 09:55:02 -0300

    Initial Commit

---
+++ FinancialManager/FinancialManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialManagerApp.swift
+//  FinancialManager
+//
+//  Created by Gabriel Lopes on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FinancialManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
